6d8a44a88efb6033ee77706f40a96f4165581a32
pythondude325 <me@gisch.dev>
2021-01-29 22:21:56 -0600

    basic rendering

--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import junk_pipe from "./assets/junk-pipe.glb";
 
 document.addEventListener("DOMContentLoaded", () => {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer({antialias: true});
 
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
+    console.log({"junk_pipe": junk_pipe});
 
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    const loader = new GLTFLoader();
+
+    function render(){
+        // requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+    }
+
+    loader.load(
+        junk_pipe,
+        gltf => {
+            gltf.scene.traverse((child) => {
+                console.log(child);
+            });
+            scene.add(gltf.scene);
+
+            render();
+        }
+    );
 
     camera.position.z = 5;
     camera.position.x = 1;
-    camera.position.y = 1;
-
-    function animate(){
-        requestAnimationFrame(animate);
-        renderer.render(scene, camera);
-    }
-    animate();
+    camera.position.y = 2;
 
 });
