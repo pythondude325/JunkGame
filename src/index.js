68edf94d96f8f68369d226a120a18d9ed0c7b8db
pythondude325 <me@gisch.dev>
2021-01-30 11:24:41 -0600

    Fixed async runtime issues

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
+import "babel-polyfill";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import junk_pipe from "./assets/junk-pipe.glb";
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
     const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xaaeeff);
+
     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
+    camera.position.x = 1;
+    camera.position.y = 2;
+
     const renderer = new THREE.WebGLRenderer({antialias: true});
 
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    console.log({"junk_pipe": junk_pipe});
+    const ambientLight = new THREE.AmbientLight( 0x404040 ); // soft white light
+    scene.add( ambientLight );
+
+    const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+    directionalLight.position.z = 0.2;
+    directionalLight.position.x = 0.2;
+    scene.add( directionalLight );
 
     const loader = new GLTFLoader();
 
@@ -19,20 +32,17 @@ document.addEventListener("DOMContentLoaded", () => {
         renderer.render(scene, camera);
     }
 
-    loader.load(
-        junk_pipe,
-        gltf => {
-            gltf.scene.traverse((child) => {
-                console.log(child);
-            });
-            scene.add(gltf.scene);
+    async function load_gltf(file_url){
+        return new Promise((res, rej) => loader.load(file_url, res, undefined, rej));
+    }
 
-            render();
-        }
-    );
+    const junk_pipe_data = await load_gltf(junk_pipe);
+    const object_models = {};
+    for(var child of junk_pipe_data.scene.children){
+        object_models[child.name] = child;
+        scene.add(child);
+    }
 
-    camera.position.z = 5;
-    camera.position.x = 1;
-    camera.position.y = 2;
+    render();
 
 });
